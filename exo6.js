8da3d1566a7886b0404de22268bb0d6b25e89039
Logan <loloherbo.drekoh@gmail.com>
2023-10-17 15:41:07 +0200

    exojs

---
+++ exo6.js
@@ -0,0 +1,16 @@
+var nom = window.prompt("Saisissez votre nom");
+var prenom = window.prompt("Saisissez votre prenom");
+
+alert(nom +prenom);
+
+var nb1 =window.prompt("Entrez le 1er nombre");
+var nb2 = window.prompt("Entrez le 2eme nombre");
+
+alert(nb1 * nb2);
+
+
+var c =
+window.prompt("Donnez-moi la temperature en celsius");
+var f = ((c*9/5)+32) ;
+
+alert("La temperature en fahrenheit est de " + f);
---
+++ test.js
@@ -0,0 +1,4 @@
+if (window.confirm("Voulez vous continuer ?") == true)
+{
+    
+}
