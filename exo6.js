e957013d08739d70b173565c8dfc64ef0b3b3e30
Logan <loloherbo.drekoh@gmail.com>
2023-10-17 16:48:47 +0200

    Exojs

--- exo6.js
+++ exo6.js
@@ -3,13 +3,13 @@ var prenom = window.prompt("Saisissez votre prenom");
 
 alert(nom +prenom);
 
-var nb1 =window.prompt("Entrez le 1er nombre");
-var nb2 = window.prompt("Entrez le 2eme nombre");
+var nb1 =parseInt(window.prompt("Entrez le 1er nombre"));
+var nb2 = parseInt(window.prompt("Entrez le 2eme nombre"));
 
 alert(nb1 * nb2);
 
 
-var c =window.prompt("Donnez-moi la temperature en celsius");
-var f = ((c*9/5)+32) ;
+var c = parseInt(window.prompt("Donnez-moi la temperature en celsius"));
+var f = parseInt(((c*9/5)+32)) ;
 
 alert("La temperature en fahrenheit est de " + f);
---
+++ exo7.js
@@ -0,0 +1,12 @@
+var a = ("100");
+var b = (100);
+var c = parseInt ((1.00));
+var d = true ;
+
+alert("Ceci est une chaine de caractere:"+a);
+ var b = --b;
+alert(b);
+var c = (c+"a");
+alert(c);
+ var d = false ;
+alert(d);
