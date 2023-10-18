ab3326f6e5c9d2b6909586813dc6d00ce9716da5
Logan <loloherbo.drekoh@gmail.com>
2023-10-18 15:19:34 +0200

    Exojs

---
+++ exo3.js
@@ -0,0 +1,4 @@
+alert("coucou");
+document.write("<p>Coucou les amis</p>")
+document.write("<h1>HEY</h1>")/*Le code HTML fonctionne avec document.write() */
+console.log("<p>hey</p>") /*Je remarque que le code HTML ne  fonctionne pas avec alert()*/
--- exo7.js
+++ exo7.js
@@ -4,9 +4,8 @@ var c = parseInt ((1.00));
 var d = true ;
 
 alert("Ceci est une chaine de caractere:"+a);
- var b = --b;
-alert(b);
-var c = (c+"a");
-alert(c);
- var d = false ;
-alert(d);
+alert(b = --b);
+
+alert(c = (c+"a"));
+var d = false ;
+alert("d ="+ d);
