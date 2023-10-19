7a337fcb906499c74a252e5a7fe84af1159c3937
Logan <loloherbo.drekoh@gmail.com>
2023-10-19 16:47:21 +0200

    Exojs

--- exo9.js
+++ exo9.js
@@ -1,51 +1,63 @@
-/**exercice 1 */
+// /**exercice 1 */
 
 
-var a=true ;
-var numero=1;
-var n=1;
-while(a==true){
+// var a=true ;
+// var numero=1;
+// var n=1;
+// while(a==true){
 
-    var prenom = prompt("Saissisez le prenom N°" + (numero++) );
+//     var prenom = prompt("Saissisez le prenom N°" + (numero++) );
 
-    console.log("prenom numero"+(n++) + prenom);
-   if ( prenom =='' || prenom == null){
-    a=false
+//     console.log("prenom numero"+(n++) + prenom);
+//    if ( prenom =='' || prenom == null){
+//     a=false
     
-   }
-} 
+//    }
+// } 
 
 
-/**exercice 2 */
-console.log(prenom* n-1)
+// /**exercice 2 */
+// console.log(prenom* n-1)
 
 
-var nombre =parseInt (window.prompt("entrez un nombre"));
+// var nombre =parseInt (window.prompt("entrez un nombre"));
 
-for(nombre; nombre>=0; --nombre ){
-    i =nombre;
-    alert(i);
-    console.log(i);
-}
+// for(nombre; nombre>=0; --nombre ){
+//     i =nombre;
+//     alert(i);
+//     console.log(i);
+// }
 
 
-/**exercice 3 */
-var i = true;
-var somme = 0;
-var moy = 0;
-var counter = 0;
-while(i==true){
-    var saisie = parseInt(window.prompt("Entrez un nombre Entier"));
-    console.log(saisie);
-    if (saisie==0){
-        i=false;
-    }
-    else {
-        somme=somme+saisie;
-        ++counter;
+// /**exercice 3 */
+// var i = true;
+// var somme = 0;
+// var moy = 0;
+// var counter = 0;
+// while(i==true){
+//     var saisie = parseInt(window.prompt("Entrez un nombre Entier"));
+//     console.log(saisie);
+//     if (saisie==0){
+//         i=false;
+//     }
+//     else {
+//         somme=somme+saisie;
+//         ++counter;
         
-    }
+//     }
+
+// }
+// var moy = somme/counter;
+// alert("La somme est : "+somme+"."+"Le nombre de saisie :"+counter+"."+"La moyenne de toutes les saisies"+moy+".");
+
+/*Exercice 4*/
+var N=0;
+var X=0;
+N = parseInt(window.prompt("Entrez le premier nombre"));
+X = parseInt(window.prompt("Entrez le deuxieme nombre"));
+
+for (N=0;N<=N;N++){
+    result=N*X;
+    alert(result);   
 
 }
-var moy = somme/counter;
-alert("La somme est : "+somme+"."+"Le nombre de saisie :"+counter+"."+"La moyenne de toutes les saisies"+moy+".");
