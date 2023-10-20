fdf0786f8a11a895ffcf917bf7ffed3b74ddaccb
Logan <loloherbo.drekoh@gmail.com>
2023-10-20 12:21:40 +0200

    Exojs

--- exo10.js
+++ exo10.js
@@ -1,20 +1,23 @@
-var html = document.getElementById("img").innerHTML;
-
-x = parseInt(window.prompt("Entrez le premier nombre"));
-y = parseInt(window.prompt("Entrez le multiplicateur:"));
 function afficheimg(image){
     if(x==null || y==null)
     html=false;
    
 }
-function produit(x , y){
-   
-
-    var m= x*y;
-    return m
+function produit(nb1,nb2){
+    var m= nb1*nb2;
+    return m;
 }
-var k =produit(2);
+
+var html = document.getElementById("img").innerHTML;
+
+var x = parseInt(window.prompt("Entrez le premier nombre"));
+var y = parseInt(window.prompt("Entrez le multiplicateur:"));
+var k =produit(x,y);
     
 
-document.write("Le produit est :"+k);
+document.getElementById("text").innerHTML = "Le produit est :"+k;
 
+
+
+
+afficheimg();
